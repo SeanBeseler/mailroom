e857f15b43958530069044c039571b8eb1c0a5fb
Chris Hudson <c.ahudson84@yahoo.com>
2017-05-18 14:50:30 -0700

    quit stuff

--- mail.py
+++ mail.py
@@ -1,6 +1,12 @@
+"""."""
+import sys
+quit = ['Q', 'Quit']
+
+
 def check_amount(p_amount):
+    """."""
     try:
-        p_amount = p_amount.replace(',','')
+        p_amount = p_amount.replace(',', '')
         p_amount = float(p_amount)
         des = "%.2f" % p_amount
         t_des = p_amount - float(des)
@@ -11,42 +17,62 @@ def check_amount(p_amount):
             return False
     except ValueError:
         return False
-def adddic(dic , name):
-    dic[name] = [0.00, 0 ,0.00]
-    return dic
-def printdon(dic):
-    for x, val in enumerate(dic):
+
+
+def adddic(donor_info, name):
+    """."""
+    donor_info[name] = [0.00, 0, 0.00]
+    return donor_info
+
+
+def printdon(donor_info):
+    """."""
+    for x, val in enumerate(donor_info):
         print()
         print(val)
 
+
 def mail():
-    dic = {'a': 1}
+    """."""
+    donor_info = {'a': 1}
     ex = True
     while(ex):
         print()
-        i1 = input("To send a thank you press 1, to create a report press 2, to quit press q: ")
-        if int(i1) == 1:
+        i1 = input("Please choose: (T)hank you, (C)reate report, (Q)uit: ")
+        if i1 == 'T':
             ex1 = True
             while(ex1):
                 print()
-                i1 = input("Please input a name, for a list of donors type list: ")
-                if i1 == 'list':
-                    dlist = list(dic.keys())
-                    printdon(dlist)
-                elif i1 == 'name':
+                i2 = input("Please input a name, for a list of donors \
+type list: ")
+                if i2 == 'list':
+                    dlist = list(donor_info.keys())
+                    print(dlist)
+                elif i2 == 'name':
                     ex2 = True
                     while ex2:
-                        amount =input("Please put in the donation amount: Must consist only numbers, for example $1,000 shoud be typed in as 1000")
+                        amount = input("""
+Please put in the donation amount: Must consist only numbers,
+(for example $1,000 shoud be typed in as 1000)
+\n> """)
                         if check_amount(amount):
                             print("here")
-                            if i1 not in dic:
+                            if i2 not in donor_info:
                                 print('hi')
+                        elif amount in quit:
+                            ex1 = False
+                            ex2 = False
                         else:
                             print("Please put in a corrrect amount")
+                elif i2 in quit:
+                    ex1 = False
 #work after this Jim
 
 
 
 #work after Chis
-                    
+        elif i1 in quit:
+            sys.exit('\nThank you, good bye!\n')
+
+
 mail()
