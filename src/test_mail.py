0aeb64f649a6bbb2c67b5ad02b8595cce4aeb0ef
SeanBeseler <seanwbeseler@gmail.com>
2017-05-18 18:08:34 -0700

    final update

--- src/test_mail.py
+++ src/test_mail.py
@@ -11,14 +11,6 @@ TEST_CHECK_AMOUNT_OUTPUT = [
     ('1,000.10', 1000.10)
 ]
 
-test_dict = {
-    '1888--I was': ['returning'],
-    'new problem.': ['I'],
-    'see Holmes': ['again,'],
-    'the blind.': ['He'],
-    'had now': ['returned'],
-    'his drug-created': ['dreams']
-}
 
 
 @pytest.mark.parametrize('amount, result', TEST_CHECK_AMOUNT_OUTPUT)
@@ -27,17 +19,18 @@ def test_check_amount(amount, result):
     from mail import check_amount
     assert check_amount(amount) == result
 
-'''
-def test_get_random():
+def add_dic_test():
     """."""
-    from trigrams import get_random
-    for n in range(1, 100):
-        assert get_random(n) in range(n)
+    from mial import add_dic
+    dic ={}
+    name = 'sean'
+    rdic{'sean':[0.00,0,0.00]}
+        assert add_dic(dic, name) == rdic
+
+def print_name_test():
+    """."""
+    from mial import print_name
+    rdic{'sean':[0.00,0,0.00]}
+        assert print_name(rdic) == 'sean'
 
 
-def test_build_words():
-    """."""
-    from trigrams import build_words
-    for n in range(10, 100):
-        assert len(build_words(n, test_dict).split()) == n
-    '''
